261895a6b24e8a213c5cf595702296251ab2f8f1
Eugene <eugeneayonga@gmail.com>
2022-09-03 21:38:18 +0300

    FInal Commit

--- chatterbox-server/app/controllers/application_controller.rb
+++ chatterbox-server/app/controllers/application_controller.rb
@@ -2,5 +2,26 @@ class ApplicationController < Sinatra::Base
   set :default_content_type, 'application/json'
   
   # Add routes
+  get '/messages' do
+    Message.order(:created_at).to_json
+  end
   
+
+  post '/messages' do
+    message = Message.create(params)
+    message.to_json
+  end
+
+  patch '/messages/:id' do
+    message = Message.find(params[:id])
+    message.update(body: params[:body])
+    message.to_json
+  end
+
+  delete '/messages/:id' do
+    message = Message.find(params[:id])
+    message.destroy
+    message.to_json
+  end
+
 end
